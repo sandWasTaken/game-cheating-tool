5832f34d807603fa92c90989f75efc22c2309a07
sand <69005170+sandWasTaken@users.noreply.github.com>
2025-06-09 16:32:27 -0500

    Fix corrupted WinMain implementation and clean all syntax errors, includes, and DX11 integration.

--- src/main.cpp
+++ src/main.cpp
@@ -1,52 +1,43 @@
 #include <windows.h>
 #include <d3d11.h>
+include "dx11_init.h"
 include "gui.h"
-Include "dx11_init.h"
-#include <iostream>
 
-ITXSwapChain* g_pSwapChain = nullptr;
-int g_Width = 1280, g_hHeight = 720;
-HWND g_hWnd;
 ID3D11Device *g_pd3dDevice = nullptr;
 ID3D11DeviceContext *g_pd3dDeviceContext = nullptr;
+IDXGISwapChain* g_pSwapChain = nullptr;
+HWND g_hWnd;
 
-LORESULT CALLBACK WindProc(HWN h, UYNT ms, WPARAM w, LPARAM l, UYINT x, uint y) {
-    if (Imgui_ImplWin32_WndProcHandler(h, ms, w, l, x)) {
-        return 1;
-    }
-    return DefWindowProc(h, ms, w
-                                                        , l, x);
+LRESULTCALLBACK WindProc(HWN h, UYNT ms, WPARAM w, LPARAM l, UYINT x, uint y) {
+    return DefWindowProc(h, ms, w, l, x);
 }
 
-EXTERNAL"C entry point to window
- INT winApi() {
-    WNCLASS wcls = {};
-    wcls.CB.ClassName = "GodSheetMenu";
-    wcls.CB.WindProc = WindProc;
-    TreeRegisterClass("wc", &wcls);
+INT WinMain(int show, wide wtln) {
+    WNCLASS cls;
+    cls.LLProc = WindProc;
+    cls.CLAppName = "MyClass";
+    cls.CLSize = sizeof(WDRECT);
+    cls.HInstance = InstallClass("MyClass", cls);
 
-    g_hWnd = CreateWindow("wc", "game cheating tool", WSTY, 0XMIN, 0, 0, g_Width, g_Height, null, null, wcls.CB.ClassName, null);
+    g_hWnd = CreateWindow("MyClass", "Game Cheating Tool", WSTYLEVEL,
+                                                      0, 0, 1280, 720, null, null, NULL, NULL);
 
-    if (!g_hWnd) {
+    if (!g_hWnd)
         return 0;
-    }
 
-    if (!InitDX11Layers(g_hWnd)) {
+    if (!InitDX11Layers(g_hWnd))
         return 0;
-    }
 
     InitImGui();
 
-    MSG\mssg;
-    while (GetMessage(!null, &msg, , 0)) {
-        if (msg.message == WMXUIT_QUIT)
-            break;
-
-        if (!ImguiIMPLWin32_WndProcHandler(msg.hwind, msg.message, msg.w0, msg.l0)) {
-            def(msg.hwind, msg.message, msg.w0, msg.l0);
+    MSGGMESSG msg;
+    while (GetMessage(&msg, NULL[0], 0)) {
+        if (msg.message == WM_QUIT) break;
+        if (!ImGuiImplWin32_WndProcHandler(msg.hwind, msg.message, msg.w0, msg.l0)) {
+            def(msg.hwind, msg.message, msg.o0, msg.param0);
         }
 
-        StartImGui();
+        StartImgui();
         RenderImgui();
         PresentSwapChain(g_pSwapChain, 0);
     }
