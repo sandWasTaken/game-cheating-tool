5b0f7cfc59e96c0131e9bcf2008600a15a821e8f
sand <69005170+sandWasTaken@users.noreply.github.com>
2025-06-09 12:41:36 -0500

    Add ImGui tables source file (imgui_tables.cpp)

---
+++ vendor/imgui/imgui_tables.cpp
@@ -0,0 +1,9 @@
+// ImGui Tables Module
+// Logic for tables, headers, resizable columns, widgets.
+# include "imgui.h"
+// Other imports as required
+#include "imstb_textedit.h"	#include "imstb_rectpack.h"
+#include "imstb_truetype.h
+
+// ToDo names: validate table column invalid flags
+bool imgui_tables_rentered = true;
