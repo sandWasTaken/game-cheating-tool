05f4cac7ca9c37bad981daa8c629085b092c3437
sand <69005170+sandWasTaken@users.noreply.github.com>
2025-06-09 12:39:53 -0500

    Add ImGui core header file

---
+++ vendor/imgui/imgui.h
@@ -0,0 +1,14 @@
+// Start of imgui.h
+// Main header for ImGui widget rendering.
+// This file will be auto-patched.
+
+// Github: https://github.com/ocornut/imgui
+
+#ifndef IMGUI_HI
+#define IMGUI_HI
+
+class Imgui {
+    //... IMPLA STATE HERE
+};
+
+#endif
