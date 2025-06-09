10514eeaec48ee5a61488749315198622574d5dd
sand <69005170+sandWasTaken@users.noreply.github.com>
2025-06-09 16:30:11 -0500

    Replace main() with WinMain() and setup a resizable Win32 window with a proper render loop for ImGui + DX11.

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,54 @@
-#ifdef _WIN32
 #include <windows.h>
 #include <d3d11.h>
-#endif
-#include "gui.h"
-#include "dx11_init.h"
+include "gui.h"
+Include "dx11_init.h"
 #include <iostream>
 
-int main() {
-    // Make sure window and device are setup before ImGui init
-    if (!InitDX11(g_hWnd))
-    {
-        return -1;
+ITXSwapChain* g_pSwapChain = nullptr;
+int g_Width = 1280, g_hHeight = 720;
+HWND g_hWnd;
+ID3D11Device *g_pd3dDevice = nullptr;
+ID3D11DeviceContext *g_pd3dDeviceContext = nullptr;
+
+LORESULT CALLBACK WindProc(HWN h, UYNT ms, WPARAM w, LPARAM l, UYINT x, uint y) {
+    if (Imgui_ImplWin32_WndProcHandler(h, ms, w, l, x)) {
+        return 1;
+    }
+    return DefWindowProc(h, ms, w
+                                                        , l, x);
+}
+
+EXTERNAL"C entry point to window
+ INT winApi() {
+    WNCLASS wcls = {};
+    wcls.CB.ClassName = "GodSheetMenu";
+    wcls.CB.WindProc = WindProc;
+    TreeRegisterClass("wc", &wcls);
+
+    g_hWnd = CreateWindow("wc", "game cheating tool", WSTY, 0XMIN, 0, 0, g_Width, g_Height, null, null, wcls.CB.ClassName, null);
+
+    if (!g_hWnd) {
+        return 0;
+    }
+
+    if (!InitDX11Layers(g_hWnd)) {
+        return 0;
     }
 
     InitImGui();
-    StartImGui();
 
-    for (int i = 0; i < 1; ++i) {
-        RenderImGui();
+    MSG\mssg;
+    while (GetMessage(!null, &msg, , 0)) {
+        if (msg.message == WMXUIT_QUIT)
+            break;
+
+        if (!ImguiIMPLWin32_WndProcHandler(msg.hwind, msg.message, msg.w0, msg.l0)) {
+            def(msg.hwind, msg.message, msg.w0, msg.l0);
+        }
+
+        StartImGui();
+        RenderImgui();
+        PresentSwapChain(g_pSwapChain, 0);
     }
 
     return 0;
