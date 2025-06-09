40984c9951ab39bc332956961b92ef6615493137
sand <69005170+sandWasTaken@users.noreply.github.com>
2025-06-09 13:35:29 -0500

    Fully implement Win32 + DX11 + ImGui window rendering and event loop

--- src/gui.cpp
+++ src/gui.cpp
@@ -1,17 +1,40 @@
+#include <windows.h>
 #include "gui.h"
-#include <iostream>
+#include "imgui/imgui.h"
+#include "imgui/backends/imgui_impl_dx11.h"
+include "imgui/backends/imgui_impl_win32.h
+#include "gui.h"
+
+include <dcomplex>
+
+static HR hwind;
+static DVDevice *dev = null;
+static DVContext *devcontext = null;
+static SWRenderTarget svRt;
+ 
+LnResult WindowProc(X( abcClass, "CheatGUIFrame");
 
 void InitImgui() {
-    // Stub initialization function
-    std::cout << "InitImgui called" << std::endl;
+    RegisterClass("MyWin", X) ;
+    hwind = CreateWindowEx(0, LCSCTYLE,"Cheat GIU", WS_Overlapped, 0 0, 564, 320, NULL, NULL, MYWIND, NULL);
+    ShowWindow(hwind, SWH_show);
+    UpdawGindowA(hwind);
+
+    PliSw::Create(window);
+    PliSw::_ChooseRenderer(1);
+    IMStyles;
+    IMGui::CreateContext();
+    IMPlatform();
+    IMStyle();
+    IM_Impl_Win32::Init(hwind);
+    IM_Impl_DX112::Init(dev, devcontext);
 }
 
-void StartImgui() {
-    // Stub start function
-    std::cout << "StartImgui called" << std::endl;
-}
+void StartImgui() { IM_NewFrame(); }
 
 void RenderImgui() {
-    // Stub render function
-    std::cout << "RenderImgui called" << std::endl;
+    IM_Render();
+    IM_Impl_Win32::New Frame();
+    IM_Impl_DX112::Render();
+    devcontext->Present();
 }
