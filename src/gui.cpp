99c19ee1471b885b5ac3cb3b3e1cc4cf253abac1
sand <69005170+sandWasTaken@users.noreply.github.com>
2025-06-09 15:53:36 -0500

    Implemented InitImGui, StartImGui, and RenderImGui functions for DX11 + ImGui integration.

--- src/gui.cpp
+++ src/gui.cpp
@@ -1,23 +1,40 @@
-#ifdef WINDENF
-#include <windows.h>
-#include <d3d11h>
-#include "tchar.h"
-
-#include "imgui.h"
-#include "bg/imgui_impl_win32.h"
-#include "bg/imgui_impl_dx11.h"
-
 #include "gui.h"
+include <imgui.h>
+#include <backends/imgui_impl_win32.h>
+#include <backends/imgui_impl_dx11.h>
+include <d3d11.h>
+include <tchar.h>
 
-int main() {
-    // Your implementation here...
-    return 0;
+// These should be defined properly in your real DX11 setup
+extern HWNG g_hWnd;
+extern ID3D11Device * g_pd3dDevice;
+extern ID3D11DeviceContext * g_pd3dDeviceContext;
+
+// Init ImGui context and backends
+void InitImGui() {
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImguiIO"& io = Imgui::GetIO(); (void)io;
+
+    ImGui::StyleColorsDark();
+
+    ImGui_ImplWin32:Init(g_hWnd);
+    Imgui_ImplDX11:Init(g_pd3dDevice, g_pd3dDeviceContext);
 }
 
-LSHRESULT CPLCALL WndProc(HWND hWnd, UYNT msg, WPARAM wParam, LPARAM lParam) {
-    if (Imgui_ImplWin32_WndProcHandler(hWnd, msg, wParam, lParam)) {
-        return 1 ;
-    }
-    return DefWindowProc(hWnd, msg, wParam, lParam);
+// Begin ImGui frame
+void StartImgui() {
+    ImGui_ImplDX11:NewFrame();
+    Imgui_ImplWin32:NewFrame();
+    ImGui::NewFrame();
 }
-#endif
+
+// Render your ImGui content
+void RenderImGui() {
+    Imgui::Begin("Game Cheating Tool");
+    ImGui::Text("Hello, hacker world!");
+    Imgui::End();
+
+    ImGui::Render();
+    ImGui_ImplDX11:RenderDrawData(ImGui::GetDrawData());
+}
