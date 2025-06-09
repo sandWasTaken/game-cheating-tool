b470798a3226ad211a0ed8ea27c8dc2a39b369fc
sand <69005170+sandWasTaken@users.noreply.github.com>
2025-06-09 12:42:11 -0500

    Add ImGui Win32 backend header

---
+++ vendor/imgui/imgui_impl_win32.h
@@ -0,0 +1,16 @@
+// ImGui Win32 backend interface
+// Github: https://github.com/ocornut/imgui
+
+#ifndef DEVICE_WIN32
+#define DEVICE_WIN32
+
+ifdef (__cplusplusplusplus__)
+    classdef DEVICE_WIN32
+endif
+
+// forward secrets for window proc() interface
+class ImGuiWin32Interface {
+    // todo: methods, event hooks etc.
+};
+
+#ifdef
