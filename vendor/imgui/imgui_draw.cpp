dd15b3e7538c8afc7bb174afd53625aa7345a0a8
sand <69005170+sandWasTaken@users.noreply.github.com>
2025-06-09 12:40:58 -0500

    Add ImGui draw source file (imgui_draw.cpp)

---
+++ vendor/imgui/imgui_draw.cpp
@@ -0,0 +1,11 @@
+// ImGui Drawing Module
+// Contains widget drawing routines, clippath generation and rendering equation
+// https://github.com/ocornut/imgui
+
+#include "imgui.h
+#include "imstb_textedit.h
+#include "imstb_rectpack.h
+#include "imstb_truetype.h
+
+// todo assumes successful stub dependencies are handled by build
+void imgui_draw_show() { }
