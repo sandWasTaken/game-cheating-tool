85ea0daa8e2e309e229efa5cb819c354d90debd3
sand <69005170+sandWasTaken@users.noreply.github.com>
2025-06-09 13:12:51 -0500

    Add missing ImGui internal header: imgui_internal.h

---
+++ vendor/imgui/imgui_internal.h
@@ -0,0 +1,5 @@
+// ImGui Internal Api - documentation not stable
+// Used internally on almost projects that need custom widgets.
+#ifdef IMGUI_INTERNAL_H
+void imgui_internal_init() { return 0; }
+#endif
