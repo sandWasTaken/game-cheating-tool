0c1db8af7a055dd11e25aabfedbdfc0456b9dafa
sand <69005170+sandWasTaken@users.noreply.github.com>
2025-06-09 12:44:55 -0500

    Update ImGui Win32 backend header (minimal placeholder version)

--- vendor/imgui/imgui_impl_win32.h
+++ vendor/imgui/imgui_impl_win32.h
@@ -1,16 +1,2 @@
-// ImGui Win32 backend interface
-// Github: https://github.com/ocornut/imgui
-
-#ifndef DEVICE_WIN32
-#define DEVICE_WIN32
-
-ifdef (__cplusplusplusplus__)
-    classdef DEVICE_WIN32
-endif
-
-// forward secrets for window proc() interface
-class ImGuiWin32Interface {
-    // todo: methods, event hooks etc.
-};
-
-#ifdef
+// ImGui Win32 backend header
+int imgui_impl_win32();
