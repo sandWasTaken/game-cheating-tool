6a384278626c139e3c190b6bb8617fc0c06e8f3d
sand <69005170+sandWasTaken@users.noreply.github.com>
2025-06-09 12:43:05 -0500

    Update Win32 backend source for ImGui

--- vendor/imgui/imgui_impl_win32.cpp
+++ vendor/imgui/imgui_impl_win32.cpp
@@ -1,12 +1,2 @@
 // ImGui Win32 backend implementation
-// https://github.com/ocornut/imgui
-
-#include "imgui.h
-#include "imgui_impl_win32.h"
-
-// TODO: real implementation here
-void imgui_impl_win32_init() {
-    // Setup Win32 context, render pass, capture events, etc.
-    // This is just a stub for logic test
-    // Real code would be sharing the windows with Imgui
-}
+int imgui_impl_win32(void) { return 0; }
