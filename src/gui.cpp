48593154fedc583fe19bafbc1add0af8a83f8a2d
sand <69005170+sandWasTaken@users.noreply.github.com>
2025-06-09 01:25:05 -0500

    Add gui.cpp for ImGui rendering stub

---
+++ src/gui.cpp
@@ -0,0 +1,18 @@
+#include "gui.h"
+int win_height = 800;
+int win_width = 1100;
+
+void InitImgui() {
+    // tod:
+}
+
+void StartImgui() {
+    // todo setup Imgui context
+    // themes layout, font, window setup etc
+    // in real code we use imgui_impl_dx11.cpp, imgui_impl_win32.cpp
+    // and window srcs/backend set up there too.
+}
+
+void RenderImgui() {
+    // todo render window with ImGui
+}
